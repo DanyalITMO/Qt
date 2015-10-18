d1f4e0ab4664f6d927af436abda4246afe22257c
Danyal <mugutdinov.danyal@yandex.ru>
2015-10-18 06:51:34 +0300

    QGridLayout

--- main.cpp
+++ main.cpp
@@ -4,28 +4,51 @@
 #include<QApplication>
 int main(int argc, char** argv)
 {
-QApplication app(argc, argv);
-QWidget wgt;
+    QApplication app(argc, argv);
+    QWidget wgt;
 
 
-QPushButton* pcmdA = new QPushButton("A");
-QPushButton* pcmdB = new QPushButton("A");
-QPushButton* pcmdC = new QPushButton("B");
-QPushButton* pcmdD = new QPushButton("C");
 
-QVBoxLayout* pvbxLayout = new QVBoxLayout;
-QHBoxLayout* phbxLayout = new QHBoxLayout;
+    QPushButton* pb0 = new QPushButton("0");
 
-phbxLayout->setMargin(5);
-phbxLayout->setSpacing(15);
-phbxLayout->addWidget(pcmdC);
-phbxLayout->addWidget(pcmdD);
-pvbxLayout->setMargin(5);
-pvbxLayout->setSpacing(15);
-pvbxLayout->addWidget(pcmdA);
-pvbxLayout->addWidget(pcmdB);
-pvbxLayout->addLayout(phbxLayout);
-wgt.setLayout(pvbxLayout);
-wgt.show();
-return app.exec();
+    QPushButton* pb1 = new QPushButton("1");
+    QPushButton* pb2 = new QPushButton("2");
+    QPushButton* pb3 = new QPushButton("3");
+
+    QPushButton* pb4 = new QPushButton("4");
+    QPushButton* pb5 = new QPushButton("5");
+    QPushButton* pb6 = new QPushButton("6");
+
+    QPushButton* pb7 = new QPushButton("7");
+    QPushButton* pb8 = new QPushButton("8");
+    QPushButton* pb9 = new QPushButton("9");
+
+    QPushButton* pb_plus = new QPushButton("+");
+    QPushButton* pb_div = new QPushButton("-");
+    QPushButton* pb_multipli = new QPushButton("*");
+
+    QGridLayout* pgrdLayout = new QGridLayout;
+    pgrdLayout->setMargin(5);
+    pgrdLayout->setSpacing(15);
+
+
+
+    pgrdLayout->addWidget(pb7, 2, 0);
+    pgrdLayout->addWidget(pb8, 2, 1);
+    pgrdLayout->addWidget(pb9, 2, 2);
+    pgrdLayout->addWidget(pb_plus, 2, 3);
+
+    pgrdLayout->addWidget(pb4, 3, 0);
+    pgrdLayout->addWidget(pb5, 3, 1);
+    pgrdLayout->addWidget(pb6, 3, 2);
+    pgrdLayout->addWidget(pb_div, 3, 3);
+
+    pgrdLayout->addWidget(pb1, 4, 0);
+    pgrdLayout->addWidget(pb2, 4, 1);
+    pgrdLayout->addWidget(pb3, 4, 2);
+    pgrdLayout->addWidget(pb_multipli, 4, 3);
+
+    wgt.setLayout(pgrdLayout);
+    wgt.show();
+    return app.exec();
 }
