e06ab0fbe292f3e7baa2af305e1d9d4027b13456
Danyal <mugutdinov.danyal@yandex.ru>
2015-10-18 06:04:16 +0300

    addLayout

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
 #include <QtGui>
+#include <QPushButton>
 #include<QBoxLayout>
-#include<QPushButton>
 #include<QApplication>
-
 int main(int argc, char** argv)
 {
 QApplication app(argc, argv);
-QWidget
-wgt;
+QWidget wgt;
+
+
 QPushButton* pcmdA = new QPushButton("A");
-QPushButton* pcmdB = new QPushButton("B");
-//Layout setup
-QBoxLayout* pbxLayout = new QBoxLayout(QBoxLayout::LeftToRight);
-pbxLayout->addWidget(pcmdA);
-pbxLayout->addStretch(1);
-pbxLayout->addWidget(pcmdB);
-wgt.setLayout(pbxLayout);
-wgt.resize(350, 40);
+QPushButton* pcmdB = new QPushButton("A");
+QPushButton* pcmdC = new QPushButton("B");
+QPushButton* pcmdD = new QPushButton("C");
+
+QVBoxLayout* pvbxLayout = new QVBoxLayout;
+QHBoxLayout* phbxLayout = new QHBoxLayout;
+
+phbxLayout->setMargin(5);
+phbxLayout->setSpacing(15);
+phbxLayout->addWidget(pcmdC);
+phbxLayout->addWidget(pcmdD);
+pvbxLayout->setMargin(5);
+pvbxLayout->setSpacing(15);
+pvbxLayout->addWidget(pcmdA);
+pvbxLayout->addWidget(pcmdB);
+pvbxLayout->addLayout(phbxLayout);
+wgt.setLayout(pvbxLayout);
 wgt.show();
 return app.exec();
 }
